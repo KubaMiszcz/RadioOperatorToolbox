d648b52cfb79e2a6258a27fb47ad1f071df1f5e8
KubaMiszcz <zielonyeufor@gmail.com>
2023-12-20 18:19:35 +0100

    works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EditProcedureTabComponent } from './components/edit-procedure-tab/edit-procedure-tab.component';
 import { TimerTabComponent } from './components/timer-tab/timer-tab.component';
-import { SettingsTabComponent } from './components/settings-tab/settings-tab.component';
 
 const routes: Routes = [
-  { path: 'edit-procedure-tab', component: EditProcedureTabComponent },
+  { path: 'edit-procedure-tab', component: TimerTabComponent },
   { path: 'timer-tab', component: TimerTabComponent },
-  { path: 'settings-tab', component: SettingsTabComponent },
+  { path: 'settings-tab', component: TimerTabComponent },
   { path: '', redirectTo: '/training-tab', pathMatch: 'full' },
-  { path: '**', component: EditProcedureTabComponent },
+  { path: '**', component: TimerTabComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { EditProcedureTabComponent } from './components/edit-procedure-tab/edit-procedure-tab.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SettingsTabComponent } from './components/settings-tab/settings-tab.component';
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    EditProcedureTabComponent,
     TimerTabComponent,
     FooterComponent,
-    SettingsTabComponent,
     ],
   imports: [
     BrowserModule,
