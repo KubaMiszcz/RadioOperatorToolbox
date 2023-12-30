af955090048bff214844b548509dc6d1599686e2
KubaMiszcz <zielonyeufor@gmail.com>
2023-12-31 00:06:01 +0100

    up

--- radio-operator-toolbox-react-native/App.js
+++ radio-operator-toolbox-react-native/App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to starsdfsdft working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        placeholder='liczby do zaszyfrowania'
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        placeholder='KLUCZ'
+        defaultValue='BACKGROUND'
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        placeholder='liczby zakodowane'
+      />
+      <Button
+  onPress={onPressLearnMore}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
     </View>
   );
 }
