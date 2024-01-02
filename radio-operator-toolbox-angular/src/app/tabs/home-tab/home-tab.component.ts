864546b55a9ba373ce0935e0fc86f9f668d5e8f2
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-02 18:55:15 +0100

    test localstorage

--- radio-operator-toolbox-angular/src/app/tabs/home-tab/home-tab.component.ts
+++ radio-operator-toolbox-angular/src/app/tabs/home-tab/home-tab.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-tab',
   templateUrl: './home-tab.component.html',
-  styleUrls: ['./home-tab.component.scss']
+  styleUrls: ['./home-tab.component.scss'],
 })
 export class HomeTabComponent {
+  myvar = '';
 
+  savels() {
+    localStorage.setItem('myvar', 'myvar123');
+  }
+
+  loadls() {
+    this.myvar = localStorage.getItem('myvar') ?? '';
+  }
 }
