56c699988d9c8043135dcb848db333ce9e31bdaa
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-02 18:08:41 +0100

    vigenere draft

--- radio-operator-toolbox-angular/src/app/app-routing.module.ts
+++ radio-operator-toolbox-angular/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsTabComponent } from './tabs/settings-tab/settings-tab.component';
 import { HomeTabComponent } from './tabs/home-tab/home-tab.component';
+import { VigenerePageComponent } from './components/vigenere-page/vigenere-page.component';
 
 const routes: Routes = [
   { path: 'sarneg-page', component: SarnegPageComponent },
+  { path: 'vigenere-page', component: VigenerePageComponent },
   { path: 'settings-tab', component: SettingsTabComponent },
   { path: 'home-tab', component: HomeTabComponent },
   { path: '', redirectTo: '/home-tab', pathMatch: 'full' },
--- radio-operator-toolbox-angular/src/app/components/vigenere-page/vigenere-page.component.ts
+++ radio-operator-toolbox-angular/src/app/components/vigenere-page/vigenere-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CODEWORDS_PL } from 'src/app/services/app-settings.service';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-vigenere-page',
@@ -6,5 +8,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./vigenere-page.component.scss']
 })
 export class VigenerePageComponent {
+  textDecoded = '1122334';
+  textEncoded = '';
+  currentCodeWord = 'BACKGROUND';
+  codeWords: string[] = [];
+  isEncoding = true;
 
+  constructor(private appService: AppService) {
+    this.codeWords = this.appService.getRandomElementsFromArray(
+      CODEWORDS_PL,
+      6
+    );
+    this.currentCodeWord = appService.getRandomElementsFromArray(
+      this.codeWords
+    )[0];
+  }
+
+  ngOnInit(): void {
+    this.encodeText();
+  }
+
+  encodeText() {
+    this.isEncoding = true;
+
+    if (!this.textDecoded) {
+      return;
+    }
+
+    this.validateWordDecoded();
+
+    this.textEncoded = '';
+    Array.from(this.textDecoded?.toString()).forEach((digit) => {
+      this.textEncoded += this.currentCodeWord[Number(digit)] ?? '';
+    });
+  }
+
+  validateWordDecoded() {
+    this.textDecoded = this.textDecoded?.toString().replace(/[^0-9]/g, '');
+  }
+
+  decodeText() {
+    this.isEncoding = false;
+
+    this.validateWordEncoded(
+      this.textEncoded,
+      Array.from(this.currentCodeWord)
+    );
+
+    this.textDecoded = '';
+    let array = Array.from(this.currentCodeWord);
+    Array.from(this.textEncoded.toString()).forEach((letter) => {
+      let idx = array.indexOf(
+        array.find((l) => l === letter.toUpperCase()) ?? ''
+      );
+      this.textDecoded += idx > 0 ? idx : '';
+    });
+  }
+
+  validateWordEncoded(string: string, allowedLetters: string[]) {
+    const allowedCharacters = new RegExp(`[^${this.currentCodeWord}]`, 'g');
+
+    this.textEncoded = this.textEncoded
+      .toUpperCase()
+      .replaceAll(allowedCharacters, '');
+  }
+
+  changeCodeword(value: string) {
+    this.currentCodeWord = value;
+    this.encodeText();
+  }
 }
