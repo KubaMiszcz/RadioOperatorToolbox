39aead340f73f0696464e3a901eff2cfa44612d7
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-02 19:10:24 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/KubaMiszcz/RadioOperatorToolbox/commit/4e9c5c31bd6c9fbf931924ec90881116a575eb0b

--- assets/application-default-data.ts
+++ assets/application-default-data.ts
@@ -8,3 +8,45 @@ export const APP_DATA_JSON: IAppData = {
         'SALTR/SALTUR?',
     ]
 };
+
+export const DIGITS = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  
+
+export const ALPHABET_EN=[
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ];
