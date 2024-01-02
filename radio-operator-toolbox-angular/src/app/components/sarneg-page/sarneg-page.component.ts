a6d6a8902ca351c1dff5abb0fcfb493ccd824598
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-02 01:11:25 +0100

    ad validaiton inputs

--- radio-operator-toolbox-angular/src/app/components/sarneg-page/sarneg-page.component.ts
+++ radio-operator-toolbox-angular/src/app/components/sarneg-page/sarneg-page.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { indexOf } from 'lodash-es';
 
 @Component({
   selector: 'app-sarneg-page',
   templateUrl: './sarneg-page.component.html',
-  styleUrls: ['./sarneg-page.component.scss']
+  styleUrls: ['./sarneg-page.component.scss'],
 })
 export class SarnegPageComponent {
+  wordDecoded = '1122334';
+  wordEncoded = 'nn';
+  currentCodeWord = 'BACKGROUND';
+  codeWords = ['BACKGROUND', 'SUPERWOMAN', 'COMBATHELP'];
 
+  constructor() {
+    this.currentCodeWord = this.codeWords[0];
+  }
+
+  ngOnInit(): void {
+    this.encode();
+  }
+
+  encode() {
+    this.validateWordDecoded();
+
+    this.wordEncoded = '';
+    Array.from(this.wordDecoded.toString()).forEach((digit) => {
+      this.wordEncoded += this.currentCodeWord[Number(digit)] ?? '';
+    });
+  }
+
+  validateWordDecoded() {
+    this.wordDecoded = this.wordDecoded.replace(/[^0-9]/g, '');
+  }
+
+  decode() {
+    this.validateWordEncoded(this.wordEncoded, Array.from(this.currentCodeWord));
+
+    this.wordDecoded = '';
+    let array = Array.from(this.currentCodeWord);
+    Array.from(this.wordEncoded.toString()).forEach((letter) => {
+      let idx = array.indexOf(
+        array.find((l) => l === letter.toUpperCase()) ?? ''
+      );
+      this.wordDecoded += idx > 0 ? idx : '';
+    });
+  }
+
+  validateWordEncoded(string: string, allowedLetters: string[]) {
+    const allowedCharacters = new RegExp(`[^${this.currentCodeWord}]`, 'g');
+
+    this.wordEncoded = this.wordEncoded.toUpperCase().replaceAll(allowedCharacters, '');
+  }
+
+  changeCodeword(value: string) {
+    this.currentCodeWord = value;
+    this.encode();
+  }
 }
