a0644119bbed2bb94ae259ba52871ca16f5f4f7d
Kuba Miszcz <KubaMiszcz@users.noreply.github.com>
2024-01-12 23:48:41 +0100

    Start with reports (#7)
    * test myvar
    
    * report draft
    
    * in progress
    
    * cleanup
    
    * in porgress
    
    * report schema draft
    
    * report generic works
    
    * draft finished

--- radio-operator-toolbox-angular/src/app/models/report.model.ts
+++ radio-operator-toolbox-angular/src/app/models/report.model.ts
@@ -2,9 +2,36 @@ export interface IReport {
   name: string;
   isFavourite?: boolean;
   isPeaceTime?: boolean;
-  elements?: [];
+  lines?: IReportLine[];
 }
 
 export class Report implements IReport {
   name = 'no-name';
+}
+
+export interface IReportLine {
+  lineHeader: string;
+  descriptionHeader: string;
+  descriptionLines: string[];
+  lineValues: IReportLineValue[];
+  isInPeacetime?: boolean;
+}
+
+export interface IReportLineValue {
+  label: string;
+  value?: any;
+  valueType?: VALUE_TYPES_ENUM;
+  valueSize?: VALUE_SIZES_ENUM;
+}
+
+export enum VALUE_TYPES_ENUM {
+  number,
+  text,
+  bool,
+}
+
+export enum VALUE_SIZES_ENUM {
+  short,
+  medium,
+  long,
 }
