96116794753db420b9fff0e4d4d2040f3dc894a9
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-12 21:51:54 +0100

    report schema draft

--- radio-operator-toolbox-angular/src/app/models/report.model.ts
+++ radio-operator-toolbox-angular/src/app/models/report.model.ts
@@ -2,9 +2,28 @@ export interface IReport {
   name: string;
   isFavourite?: boolean;
   isPeaceTime?: boolean;
-  elements?: [];
+  lines?: IReportLine[];
 }
 
 export class Report implements IReport {
   name = 'no-name';
+}
+
+export interface IReportLine {
+  lineHeader: string;
+  descriptionHeader: string;
+  descriptionLines: string[];
+  lineValues: IReportLineValue[];
+}
+
+export interface IReportLineValue {
+  label: string;
+  value?: any;
+  valueType: valueTypesEnum;
+}
+
+export enum valueTypesEnum {
+  number,
+  text,
+  bool,
 }
