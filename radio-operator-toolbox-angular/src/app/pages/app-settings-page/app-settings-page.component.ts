30c8ffff5cb54a1380804cc4f70665d6cae6d6ec
KubaMiszcz <zielonyeufor@gmail.com>
2024-01-29 20:39:48 +0100

    refactor

--- radio-operator-toolbox-angular/src/app/pages/app-settings-page/app-settings-page.component.ts
+++ radio-operator-toolbox-angular/src/app/pages/app-settings-page/app-settings-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { forward } from 'mgrs';
+import { AppSettingsService } from 'src/app/services/app-settings.service';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-app-settings-page',
   templateUrl: './app-settings-page.component.html',
-  styleUrls: ['./app-settings-page.component.scss']
+  styleUrls: ['./app-settings-page.component.scss'],
 })
 export class AppSettingsPageComponent {
-
+  constructor(
+    private appService: AppService,
+    private appSettingsService: AppSettingsService
+  ) {
+    this.appService.getPositionLatiLong().then((pos) => {
+      console.log(`Positon lat lon: ${pos.long} ${pos.lati}`);
+      console.log(
+        `MGRS prec5: ${this.appService.LatiLong2MGRS(pos.lati, pos.long, 5)}`
+      );
+      console.log(
+        `MGRS prec3 100m: ${this.appService.LatiLong2MGRS(
+          pos.lati,
+          pos.long,
+          3
+        )}`
+      );
+    });
+  }
 }
--- radio-operator-toolbox-angular/src/app/pages/reports-page/reports-page.component.ts
+++ radio-operator-toolbox-angular/src/app/pages/reports-page/reports-page.component.ts
@@ -21,24 +21,12 @@ export class ReportPageComponent {
   //todo refactor layout of reports
   constructor(
     private appService: AppService,
-    private appSettingsService: AppSettingsService,
-    
+    private appSettingsService: AppSettingsService
   ) {
     this.appService.currenReportBS.subscribe((r) => (this.report = r));
-
-    this.appService.getPositionLatiLong().then(pos=>
-      {
-         console.log(`Positon: ${pos.long} ${pos.lati}`);
-         console.log(`MGRS222: ${forward([pos.long, pos.lati])}`);
-         console.log(`MGRS222: ${forward([pos.long, pos.lati],3)}`);
-         console.log(`MGRS: ${this.appService.LatiLong2MGRS(pos.lati,pos.long, 5)}`);
-         console.log(`MGRS: ${this.appService.LatiLong2MGRS(pos.lati,pos.long, 3)}`);
-      });
   }
 
   getNotEmptyLineValues(lineValues: IReportLineValue[]): IReportLineValue[] {
     return lineValues.filter((lv) => !!lv.value);
   }
-
-
 }
