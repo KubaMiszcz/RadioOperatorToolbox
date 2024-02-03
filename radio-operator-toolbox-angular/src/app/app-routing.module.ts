fdfd6364ecf632967a7acb02e2b09716c6ca6235
KubaMiszcz <zielonyeufor@gmail.com>
2024-02-03 15:33:16 +0100

    move files

--- radio-operator-toolbox-angular/src/app/app-routing.module.ts
+++ radio-operator-toolbox-angular/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeTabComponent } from './pages/home-page/home-page.component';
-import { SarnegTabComponent } from './components/sarneg-tab/sarneg-tab.component';
-import { VigenereTabComponent } from './components/vigenere-tab/vigenere-tab.component';
+import { SarnegTabComponent } from './pages/encodings-page/sarneg-tab/sarneg-tab.component';
+import { VigenereTabComponent } from './pages/encodings-page/vigenere-tab/vigenere-tab.component';
 import { CorrespondenceBookPageComponent } from './pages/correspondence-book-page/correspondence-book-page.component';
 import { TDRDataPageComponent as TDRDataPageComponent } from './pages/tdr-data-page/tdr-data-page.component';
 import { AppSettingsPageComponent } from './pages/app-settings-page/app-settings-page.component';
---
+++ radio-operator-toolbox-angular/src/app/pages/home-page/menu-tile/menu-tile.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tile',
+  templateUrl: './menu-tile.component.html',
+  styleUrls: ['./menu-tile.component.scss'],
+})
+export class MenuTileComponent {
+  @Input() name = 'no-name';
+  @Input() iconName = '';
+  @Input() isLabelShowed = false;
+  
+  constructor() {
+    
+  }
+  
+  ngOnInit(): void {
+    this.iconName = (this.iconName?.length ? this.iconName : this.name) + '-image.png';
+  }
+}
