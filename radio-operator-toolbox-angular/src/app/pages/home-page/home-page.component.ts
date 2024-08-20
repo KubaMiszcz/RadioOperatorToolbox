e1ec04596a3c280301a1ce11bee037d316010f20
KubaMiszcz <zielonyeufor@gmail.com>
2024-08-20 21:13:14 +0200

    tiles as array

--- radio-operator-toolbox-angular/src/app/pages/home-page/home-page.component.ts
+++ radio-operator-toolbox-angular/src/app/pages/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { IMenuTile_V2 } from './../../models/menu-item.model';
 import { AppDataService } from 'src/app/services/app-data.service';
 import { AppService } from '../../services/app.service';
 import { Component } from '@angular/core';
@@ -12,12 +13,31 @@ export class HomePageComponent {
   appData = this.appDataService.appData;
   appSettings = this.appSettingsService.appSettings;
   helpLines = helpLines;
+  menuTiles: IMenuTile_V2[] = [];
+  favouritesTilesRows: IMenuTile_V2[][] = [];
+  tilesRows: IMenuTile_V2[][] = [];
 
-  constructor(
-    private appService: AppService,
-    private appDataService: AppDataService,
-    private appSettingsService: AppSettingsService
-  ) {}
+  constructor(private appService: AppService, private appDataService: AppDataService, private appSettingsService: AppSettingsService) {
+    this.tilesRows = this.getTilesRows(
+      appSettingsService.appSettings.menuTiles.filter((t) => !t.isFavourite),
+      3
+    );
+    this.favouritesTilesRows = this.getTilesRows(
+      appSettingsService.appSettings.menuTiles.filter((t) => t.isFavourite),
+      3
+    );
+  }
+
+  private getTilesRows(allTiles: IMenuTile_V2[], rowSize: number): IMenuTile_V2[][] {
+    let result = [];
+
+    while (allTiles.length) {
+      let row = allTiles.splice(0, rowSize);
+      result.push(row);
+    }
+
+    return result;
+  }
 
   saveAppData() {
     this.appDataService.saveAppData();
