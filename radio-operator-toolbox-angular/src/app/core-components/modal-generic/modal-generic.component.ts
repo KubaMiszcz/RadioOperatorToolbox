b44bfddbb6b8545d90c630daae413bcbb0d2f4a2
KubaMiszcz <zielonyeufor@gmail.com>
2024-08-29 22:13:08 +0200

    lint

--- radio-operator-toolbox-angular/src/app/core-components/modal-generic/modal-generic.component.ts
+++ radio-operator-toolbox-angular/src/app/core-components/modal-generic/modal-generic.component.ts
@@ -7,28 +7,26 @@ import { MODAL_RESULT } from 'src/app/models/constants/enums';
   styleUrls: ['./modal-generic.component.scss'],
 })
 export class ModalGenericComponent {
-@Output() modalResult = new EventEmitter<MODAL_RESULT>();
-@Input() title = '';
-@Input() content = '';
-// @Input() modalResults = [MODAL_RESULT.YES, MODAL_RESULT.NO];
-@Input() modalResults = [MODAL_RESULT.YES, MODAL_RESULT.NO, MODAL_RESULT.CLOSE, MODAL_RESULT.CANCEL];
+  @Output() modalResult = new EventEmitter<MODAL_RESULT>();
+  @Input() title = '';
+  @Input() content = '';
+  // @Input() modalResults = [MODAL_RESULT.YES, MODAL_RESULT.NO];
+  @Input() modalResults = [MODAL_RESULT.YES, MODAL_RESULT.NO, MODAL_RESULT.CLOSE, MODAL_RESULT.CANCEL];
 
-MODAL_RESULTS = MODAL_RESULT;
+  MODAL_RESULTS = MODAL_RESULT;
 
-constructor() {
-}
+  constructor() {}
 
+  isButtonVisible(button: MODAL_RESULT): boolean {
+    // return Array(this.MODAL_RESULTS).some((m) => String(m) === arg0.toString()); //km
+    let result = this.modalResults.some((r) => r === button);
+    console.log(result);
 
-isButtonVisible(button: MODAL_RESULT): boolean {
-  // return Array(this.MODAL_RESULTS).some((m) => String(m) === arg0.toString()); //km
-  let result = this.modalResults.some((r) => r === button);
-console.log(result);
+    return result;
+    // if (Object.values(this.MODAL_RESULTS).includes(result)) {
+    //   return true;
+    // }
 
-return result
-  // if (Object.values(this.MODAL_RESULTS).includes(result)) {
-  //   return true;
-  // }
-
-  // return false;
-}
+    // return false;
+  }
 }
